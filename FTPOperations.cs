b8ec48f5ce435f01acc1301d628f658b80aad84f
shankze <shankze@gmail.com>
2019-03-12 15:17:53 -0500

    Create FTP Operation

---
+++ FTPOperations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using Renci.SshNet;
+
+namespace PhysicalRecordsConnector.Implementation
+{
+    class FTPOperations
+    {
+        public FTPOperations() { }
+
+        public bool UploadToSiteSFTP(string sourceFilePath, string destinationUrl,string destFolderPath, NetworkCredential credentials, string fileName)
+        {
+            SftpClient client = new SftpClient(destinationUrl, 22, credentials.UserName, credentials.Password);
+            client.Connect();
+            var fileStream = new FileStream(sourceFilePath,FileMode.Open);
+            client.BufferSize = 4 * 1024;
+            client.UploadFile(fileStream, destFolderPath + fileName, null);
+            client.Dispose();
+            fileStream.Close();
+            return true;
+
+        }
+    }
+}
